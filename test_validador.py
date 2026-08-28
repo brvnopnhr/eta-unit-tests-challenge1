f03ed4d79d21ada052a8bd1cdc8046f817846024
Bruno Pinheiro <brunopnhro@gmail.com>
2026-08-27 21:29:31 -0400

    testes cpf

--- test_validador.py
+++ test_validador.py
@@ -61,3 +61,59 @@ def test_validar_cep_retorna_false_para_cep_invalido(validador, cep_invalido):
 def test_validar_cep_levanta_value_error_para_valor_nao_texto(validador, valor_nao_texto):
     with pytest.raises(ValueError):
         validador.validar_cep(valor_nao_texto)
+
+# ----------------------------------------------------------------------
+# CPF - casos válidos
+# ----------------------------------------------------------------------
+
+@pytest.mark.parametrize(
+    "cpf_valido",
+    [
+        "529.982.247-25",  # com máscara
+        "52998224725",     # sem máscara
+        "111.444.777-35",  # com máscara
+        "11144477735",     # sem máscara
+    ],
+)
+def test_validar_cpf_retorna_true_para_cpf_valido(validador, cpf_valido):
+    assert validador.validar_cpf(cpf_valido) is True
+
+
+# ----------------------------------------------------------------------
+# CPF - casos inválidos
+# ----------------------------------------------------------------------
+
+@pytest.mark.parametrize(
+    "cpf_invalido",
+    [
+        "111.111.111-11",  # dígitos todos repetidos
+        "123.456.789-00",  # dígitos verificadores incorretos
+        "529.982.247-26",  # último dígito verificador alterado
+        "5299822472",      # tamanho incorreto (faltando um dígito)
+        "529982247255",    # tamanho incorreto (dígito a mais)
+        "ABC.DEF.GHI-JK",  # não numérico
+        "",                 # vazio
+    ],
+)
+def test_validar_cpf_retorna_false_para_cpf_invalido(validador, cpf_invalido):
+    assert validador.validar_cpf(cpf_invalido) is False
+
+
+# ----------------------------------------------------------------------
+# CPF - exceção ValueError
+# ----------------------------------------------------------------------
+
+@pytest.mark.parametrize(
+    "valor_nao_texto",
+    [
+        52998224725,
+        52998224725.0,
+        None,
+        ["529.982.247-25"],
+        {"cpf": "529.982.247-25"},
+    ],
+)
+def test_validar_cpf_levanta_value_error_para_valor_nao_texto(validador, valor_nao_texto):
+    with pytest.raises(ValueError):
+        validador.validar_cpf(valor_nao_texto)
+
